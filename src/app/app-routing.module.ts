e8ce145f535e8464dec4c8361b7c03d2f5009a35
jampoolG <jampoolgarcia@gmail.com>
2021-07-06 12:07:42 -0400

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
+//core modules
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// compoennts
+import { BudgetComponent } from './budget/budget.component';
+import { SpendingComponent } from './spending/spending.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/budget'
+  },
+  {
+    path: 'budget',
+    component: BudgetComponent
+  },
+  {
+    path: 'spending',
+    component: SpendingComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/budget'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+// core modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+// routing
 import { AppRoutingModule } from './app-routing.module';
+
+// root
 import { AppComponent } from './app.component';
+
+// components
 import { BudgetComponent } from './budget/budget.component';
 import { SpendingComponent } from './spending/spending.component';
 import { FormComponent } from './spending/form/form.component';
@@ -10,14 +17,21 @@ import { ListComponent } from './spending/list/list.component';
 
 @NgModule({
   declarations: [
+    // root
     AppComponent,
+
+    // components
     BudgetComponent,
     SpendingComponent,
     FormComponent,
     ListComponent
   ],
   imports: [
+    // core
     BrowserModule,
+    FormsModule,
+
+    // routing
     AppRoutingModule
   ],
   providers: [],
